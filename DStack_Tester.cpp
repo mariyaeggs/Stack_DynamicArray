db03448e27f7dacf168965e27d4e0f9cb36f7222
mariyaeggs <mariyawilson@live.com>
2017-01-15 14:13:47 -0500

    Finalize testing of stack.

--- DStack_Tester.cpp
+++ DStack_Tester.cpp
@@ -20,23 +20,26 @@ using namespace std;
  * @author Mariya Eggensperger
 */
 int main() {
-   Stack dynamic_stack;
+   Stack DStack;
    cout << "\n\nStack created. Empty? "
-        << boolalpha << dynamic_stack.is_empty() << endl; // Print whether stack is empty
+        << boolalpha << DStack.is_empty() << endl; // Print whether stack is empty
 
    cout << "\nEnter how many elements to add to the stack : ";
    int numItems;
    cin >> numItems; // User input of element count
    for(int i=1; i <= numItems; i++) {
-      dynamic_stack.push(i); // Insert user's elements into stack
+      DStack.push(i); // Insert user's elements into stack
    }
    cout << "\nStack items: ";
-   dynamic_stack.display(cout);
-   cout << "\nStack empty? " << dynamic_stack.is_empty() << endl;
-   cout << "\nTop item: " << dynamic_stack.top() << endl;
-   while(!dynamic_stack.is_empty()) { // While the stack is not empty
-      cout << "\nPop: " << dynamic_stack.top() << endl;
-      dynamic_stack.pop();
+   DStack.display(cout);
+   cout << "\nStack empty? " << DStack.is_empty() << endl;
+   cout << "\nTop item: " << DStack.top() << endl;
+   while(!DStack.is_empty()) { // While the stack is not empty
+      cout << "\nPop: " << DStack.top() << endl;
+      DStack.pop();
    }
-   cout << "\nStack empty? "
+   cout << "\nStack empty? " << DStack.is_empty() << enld;
+   cout << "\nTop item: " << DStack.top() << endl;
+   cout << "\nPop: "<< endl;
+   DStack.pop();
 }
