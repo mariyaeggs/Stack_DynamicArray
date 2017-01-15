b9ee8d0003fd6ce05062c44a1a651e05f6529d73
mariyaeggs <mariyawilson@live.com>
2017-01-15 12:25:37 -0500

    Insertion of fundamental files to run program.

---
+++ cmake-build-debug/CMakeFiles/Stacks_DynamicArray.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stacks_DynamicArray.dir/main.cpp.o"
+  "Stacks_DynamicArray.pdb"
+  "Stacks_DynamicArray"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stacks_DynamicArray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
